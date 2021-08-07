ca7cd42b47e95dec1444031b3dc88e7ad9199c5b
Полевая Мышь <86875824+fieldMouse97@users.noreply.github.com>
2021-08-07 08:34:35 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-function reverse (anyInt) {
-  let changedInt = anyInt
+module.exports = function reverse (n) {
+  let changedInt = n
   let suppInt = 0
   let helpingInt
   while (changedInt) {
     helpingInt = changedInt % 10
     suppInt = suppInt * 10 + helpingInt
-    changedInt = changedInt / 10 | 0
+    changedInt = Math.floor(changedInt / 10)
   }
   return suppInt
 }
